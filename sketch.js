82ade0eaba2e58b33b66ca2ccfc997281ae249cd
a-man-named-karma <77083703+a-man-named-karma@users.noreply.github.com>
2021-03-31 20:59:12 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var balloon, database;
+var position;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  balloon = createSprite(250,250,10,10);
+  balloon.shapeColor = "red";
+
+
+  var balloonPosition = database.ref('balloon/position');
+  balloonPosition.on("value", readPosition, showError);
+}
+
+function draw(){
+  background("white");
+  
+    if(keyDown(LEFT_ARROW)){
+      writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+      writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+      writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+      writePosition(0,+1);
+    }
+    drawSprites();
+  
+}
+
+function writePosition(x,y){
+  database.ref('balloon/position').set({
+    'x': position.x + x ,
+    'y': position.y + y
+  })
+}
+
+function readPosition(data){
+  position = data.val();
+  console.log(position.x);
+  balloon.x = position.x;
+  balloon.y = position.y;
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}
